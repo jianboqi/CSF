f45fc9e4c5565a4a273fb81334f5ae4ce7472766
jianboqi@126.com <jianboqi@126.com>
2016-06-25 11:43:59 +0800

    添加CSFDemo.cpp

---
+++ CSFDemo/CSFDemo.cpp
@@ -0,0 +1,74 @@
+// SimpleDLLTest.cpp : 定义控制台应用程序的入口点。
+
+#include <vector>
+#include "Cfg.h"
+#include "../CSFDLL/CSF.h" 
+#pragma comment(lib, "../Release/CSFDLL.lib")
+#include <process.h>
+#include <locale.h>
+using namespace std;
+
+int main()
+{
+	//读取文本参数，仅用于调试
+	Cfg cfg;
+	string slop_smooth;
+	cfg.readConfigFile("params.cfg", "slop_smooth", slop_smooth);
+	bool ss = false;
+	if (slop_smooth == "true" || slop_smooth == "True")
+	{
+		ss = true;
+	}
+	else if (slop_smooth == "false" || slop_smooth == "False")
+	{
+		ss = false;
+	}
+	else{
+		if (atoi(slop_smooth.c_str()) == 0){
+			ss = false;
+		}
+		else
+		{
+			ss = true;
+		}
+	}
+
+	string class_threshold;
+	cfg.readConfigFile("params.cfg", "class_threshold", class_threshold);
+	string cloth_resolution;
+	cfg.readConfigFile("params.cfg", "cloth_resolution", cloth_resolution);
+	string iterations;
+	cfg.readConfigFile("params.cfg", "iterations", iterations);
+	string k_nearest_points;
+	cfg.readConfigFile("params.cfg", "k_nearest_points", k_nearest_points);
+	string rigidness;
+	cfg.readConfigFile("params.cfg", "rigidness", rigidness);
+	string time_step;
+	cfg.readConfigFile("params.cfg", "time_step", time_step);
+	string terr_pointClouds_filepath;
+	cfg.readConfigFile("params.cfg", "terr_pointClouds_filepath", terr_pointClouds_filepath);
+
+	CSF csf;
+	//step 1 输入点云
+	csf.readPointsFromFile(terr_pointClouds_filepath);
+
+	//备注：在实际使用过程中，点云数据由主程序提供，调用函数为
+	//csf.setPointCloud(pc);//pc为PointCloud类
+
+	//step 2 设置参数
+	csf.params.bSloopSmooth = ss;
+	csf.params.class_threshold = atof(class_threshold.c_str());
+	csf.params.cloth_resolution = atof(cloth_resolution.c_str());
+	csf.params.interations = atoi(iterations.c_str());
+	csf.params.k_nearest_points = atoi(k_nearest_points.c_str());
+	csf.params.rigidness = atoi(rigidness.c_str());
+	csf.params.time_step = atof(time_step.c_str());
+
+	//step3 执行滤波,result中储存的是地面点的索引 
+	vector<int> result = csf.do_filtering();
+
+
+	csf.saveGroundPoints(result);
+
+	return 0;
+}
