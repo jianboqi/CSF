6119caa4e342093ddcbf2d1019b540e9502c0a71
jianboqi@126.com <jianboqi@126.com>
2016-06-26 14:20:55 +0800

    优化计算constraint

--- CSFDLL/Particle.cpp
+++ CSFDLL/Particle.cpp
@@ -25,18 +25,18 @@ void Particle::satisfyConstraintSelf(int constraintTimes)
 		Vec3 correctionVector(0, p2->pos.f[1] - p1->pos.f[1], 0);
 		if (p1->isMovable() && p2->isMovable())
 		{
-			Vec3 correctionVectorHalf = correctionVector * (constraintTimes>14 ? 0.5 : doubleMove1[constraintTimes - 1]); // Lets make it half that length, so that we can move BOTH p1 and p2.
+			Vec3 correctionVectorHalf = correctionVector * (constraintTimes>14 ? 0.5 : doubleMove1[constraintTimes]); // Lets make it half that length, so that we can move BOTH p1 and p2.
 			p1->offsetPos(correctionVectorHalf);
 			p2->offsetPos(-correctionVectorHalf);
 		}
 		else if (p1->isMovable() && !p2->isMovable())
 		{
-			Vec3 correctionVectorHalf = correctionVector * (constraintTimes>14 ? 1 : singleMove1[constraintTimes - 1]); // Lets make it half that length, so that we can move BOTH p1 and p2.
+			Vec3 correctionVectorHalf = correctionVector * (constraintTimes>14 ? 1 : singleMove1[constraintTimes]); // Lets make it half that length, so that we can move BOTH p1 and p2.
 			p1->offsetPos(correctionVectorHalf);
 		}
 		else if (!p1->isMovable() && p2->isMovable())
 		{
-			Vec3 correctionVectorHalf = correctionVector * (constraintTimes>14 ? 1 : singleMove1[constraintTimes - 1]); // Lets make it half that length, so that we can move BOTH p1 and p2.
+			Vec3 correctionVectorHalf = correctionVector * (constraintTimes>14 ? 1 : singleMove1[constraintTimes]); // Lets make it half that length, so that we can move BOTH p1 and p2.
 			p2->offsetPos(-correctionVectorHalf);
 		}
 	}
