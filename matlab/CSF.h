9d56f86bee572fc7f18c4166d5196414b7bd7a30
jianboqi <jianboqi@126.com>
2016-06-28 08:37:47 +0800

    添加Matlab支持

---
+++ matlab/CSF.h
@@ -0,0 +1,93 @@
+﻿
+//#######################################################################################
+//#                                                                                     #
+//#                              CLOUDCOMPARE PLUGIN: qCSF                              #
+//#                                                                                     #
+//#  Please cite the following paper, If you use this plugin in your work.              #
+//#                                                                                     #
+//#  Zhang W, Qi J, Wan P, Wang H, Xie D, Wang X, Yan G. An Easy-to-Use Airborne LiDAR  #
+//#  Data Filtering Method Based on Cloth Simulation. Remote Sensing. 2016; 8(6):501.   #
+//#                                                                                     #
+//#                                     Copyright ©                                     #
+//#               RAMM laboratory, School of Geography, Beijing Normal University       #
+//#                               (http://ramm.bnu.edu.cn/)                             #
+//#                                                                                     #
+//#                      Wuming Zhang; Jianbo Qi; Peng Wan; Hongtao Wang                #
+//#                                                                                     #
+//#                      contact us: 2009zwm@gmail.com; wpqjbzwm@126.com                #
+//#                                                                                     #
+//#######################################################################################
+
+
+
+//cloth simulation filter for airborne lidar filtering
+#ifndef _CSF_H_
+#define _CSF_H_
+
+#include "point_cloud.h"
+#include <iostream>
+using namespace wl;
+
+
+#pragma once;
+
+//该宏完成在dll项目内部使用__declspec(dllexport)导出
+//在dll项目外部使用时，用__declspec(dllimport)导入
+//宏DLL_IMPLEMENT在CSF.cpp中定义
+//#ifdef DLL_IMPLEMENT  
+//#define DLL_API __declspec(dllexport)  
+//#else  
+//#define DLL_API __declspec(dllimport)  
+//#endif
+
+class /*DLL_API*/  CSF
+{
+public:
+	CSF();
+	~CSF();
+
+	//设置点云数据 从vector导入点云 set pointcloud from vector
+	void setPointCloud(vector< LASPoint > points);
+	//从文件读取点云 主要用于测试  read pointcloud from txt file: (X Y Z) for each line
+	void readPointsFromFile(string filename);
+	//保存地面点到文件 调试用 save extracted ground points to file
+	void saveGroundPoints(vector<int> grp, string path = "");
+
+	//get size of pointcloud
+	size_t size(){return point_cloud.size();}
+
+
+	//从已有的PointCloud中输入  set pointcloud 
+	void setPointCloud(PointCloud &pc);
+
+	//执行滤波处理 得到地面点的在PointCloud 中的序号  
+	//do filtering, the results are index of ground points in the original pointcloud
+	vector<int> do_filtering();
+
+private:
+	 /*class __declspec (dllexport)*/ wl::PointCloud point_cloud;
+
+public:
+
+	struct{
+
+		//是否进行边坡后处理
+		bool bSloopSmooth;
+		//时间步长
+		double time_step;
+
+		//分类阈值
+		double class_threshold;
+
+		//布料格网大小
+		double cloth_resolution;
+
+		//布料硬度参数
+		int rigidness;
+
+		//迭代次数
+		int interations;
+	}params;
+};
+
+#endif
---
+++ matlab/csf_filtering.cpp
@@ -0,0 +1,48 @@
+// SimpleDLLTest.cpp : 定义控制台应用程序的入口点。
+#include <mex.h>
+#include <vector>
+#include <string>
+#include "CSF.h" 
+using namespace std;
+#pragma comment(lib, "CSF.lib")
+
+//input: pointcloudfilepath riginess isSlopSmooth cloth_resolution 
+void csf_filtering(string filepath,int rigidness, bool isSmooth, double cloth_resolution)
+{
+
+	CSF csf;
+	//step 1 read point cloud
+	
+	csf.readPointsFromFile(filepath);
+
+	//备注：在实际使用过程中，点云数据由主程序提供，调用函数为
+	//csf.setPointCloud(pc);//pc为PointCloud类
+
+	//step 2 设置参数
+	csf.params.bSloopSmooth = isSmooth;
+	csf.params.class_threshold = 0.5;
+	csf.params.cloth_resolution = cloth_resolution;
+	csf.params.interations = 500;
+	csf.params.rigidness = rigidness ;
+	csf.params.time_step = 0.65;
+
+	//step3 执行滤波,result中储存的是地面点的索引 
+	vector<int> result = csf.do_filtering();
+
+	csf.saveGroundPoints(result);
+	mexPrintf("OK, Done!\n");
+}
+
+void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[])
+{
+	char filepath[200];
+	mxGetString(prhs[0],filepath,200);
+	double resolution = mxGetScalar(prhs[3]);
+	bool isSmooth =  mxIsLogicalScalarTrue(prhs[2]);
+	int rigidness = (int)mxGetScalar(prhs[1]);
+
+	csf_filtering(filepath,rigidness,isSmooth,resolution);
+
+}
+
+
