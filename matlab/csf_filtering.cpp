a9392c1f5dee4d8eee10e9b936e4405c2af0c87b
jianboqi <jianboqi@126.com>
2016-06-28 08:52:58 +0800

    add m file

--- matlab/csf_filtering.cpp
+++ matlab/csf_filtering.cpp
@@ -1,3 +1,4 @@
+
 // SimpleDLLTest.cpp : 定义控制台应用程序的入口点。
 #include <mex.h>
 #include <vector>
---
+++ matlab/csf_filtering.m
@@ -0,0 +1,10 @@
+function csf_filtering(PointCloudFilePath,rigidness,isSmooth,clothResolution)
+%CSF_FILTERING Filtering ground points from lidar point cloud
+%   PointCloudFilePath file path to the point cloud
+%   rigidness the regidness of the cloth,1 for tilted terrain, 2 for terrain with gentle slop, 3 for city areas with flat terrain
+%   isSmooth is post processing 
+%   clothResolution grid size of cloth
+
+
+end
+
