4d05bbd8593c45f3948772e7aef0f56c32e7e5f3
Jianbo Qi <jianboqi@gmail.com>
2019-02-27 18:59:07 +0800

    Change lowest height value for pacticle in empty area

--- src/CSF.cpp
+++ src/CSF.cpp
@@ -156,8 +156,8 @@ void CSF::do_filtering(std::vector<int>& groundIndexes,
     for (int i = 0; i < params.interations; i++) {
         double maxDiff = cloth.timeStep();
         cloth.terrCollision();
-
-        if ((maxDiff != 0) && (maxDiff < params.class_threshold / 100)) {
+		//params.class_threshold / 100
+        if ((maxDiff != 0) && (maxDiff < 0.005)) {
             // early stop
             break;
         }
